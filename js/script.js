fc6dd548223dc8a2f3c018a80a7f86e01391ea0b
Adam Sadkowski <adam92as@gmail.com>
2019-06-04 22:39:31 +0200

    exercise next exercise - function titleClickHandler add 'remove class'

--- js/script.js
+++ js/script.js
@@ -6,7 +6,19 @@ function titleClickHandler(event){
 
   /* remove class 'active' from all article links  */
 
+const activeLinks = document.querySelectorAll('.titles a.active');
+
+for(let activeLink of activeLinks){
+  activeLink.classList.remove('active');
+}
+
   /* add class 'active' to the clicked link */
+
+const activeArticles = document.querySelectorAll('.post');
+
+for(let activeArticle of activeArticles){
+  activeArticle.classList.remove('active');
+}
 
   /* remove class 'active' from all articles */
 
