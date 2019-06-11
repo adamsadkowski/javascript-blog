86b70cb608a0d2f6c4486d3b5ac249b491274e5b
Adam Sadkowski <adam92as@gmail.com>
2019-06-11 22:17:52 +0200

    add class 'active' for script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 'use strict';
-
+const clickedElement = this;
 function titleClickHandler(event){
   console.log('Link was clicked!');
   console.log(event);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
 
 const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -12,15 +12,19 @@ for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
 
-  /* add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
+  const clickedElement = this;
+
+  clickedElement.classList.add('active')
+  console.log('clickedElement (with plus): ' + clickedElement);
+
+  /* [DONE] remove class 'active' from all articles */
 
 const activeArticles = document.querySelectorAll('.post');
 
 for(let activeArticle of activeArticles){
   activeArticle.classList.remove('active');
 }
-
-  /* remove class 'active' from all articles */
 
   /* get 'href' attribute from the clicked link */
 
